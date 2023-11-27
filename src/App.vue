b6f0046e1bfcb1fdc44b54e9302c3d9bbcdf1c71
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2023-11-27 16:26:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from "axios";
+import { store } from "./store.js";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  created() {
+    axios
+      .get(this.store.apiCards)
+      .then((resp) => {
+        this.store.cards = resp.data;
+    });
+  },
+  components: {},
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+</style>
---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    apiCards: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
+    cards: [],
+})
